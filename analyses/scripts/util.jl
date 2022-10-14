ab6a6ac6e5f8dfc1e87a3d9306fb130c29f348b2
dpmerrell <davidpmerrell@gmail.com>
2022-10-14 00:55:07 -0500

    Reversed "is away" to "is home"

--- analyses/scripts/util.jl
+++ analyses/scripts/util.jl
@@ -5,9 +5,8 @@ function unpack_df(train_df)
     date_vec = train_df[:,:Date]
     a_scores = train_df[:,:ScoreA]
     b_scores = train_df[:,:ScoreB]
-    # TODO reverse these
-    a_covariates = Matrix{Float32}(train_df[:,[:IsHomeB]])
-    b_covariates = Matrix{Float32}(train_df[:,[:IsHomeA]])
+    a_covariates = Matrix{Float32}(train_df[:,[:IsHomeA]])
+    b_covariates = Matrix{Float32}(train_df[:,[:IsHomeB]])
     return team_a_vec, team_b_vec, a_scores, b_scores, date_vec, a_covariates, b_covariates
 end
 
